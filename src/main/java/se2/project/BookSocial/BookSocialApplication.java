fdd5a90f8f7f137c1ea9ed2795b32d23a6056361
mhquang <mhquang03@gmail.com>
2024-05-07 22:18:21 +0700

    initial commit

---
+++ src/main/java/se2/project/BookSocial/BookSocialApplication.java
@@ -0,0 +1,13 @@
+package se2.project.BookSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se2/project/BookSocial/BookSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package se2.project.BookSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
