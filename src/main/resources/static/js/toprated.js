53156fa37a3126daf05d0c9f817fe14f4219fb5a
Buiduc03 <bduc21112003@gmail.com>
2024-05-15 02:07:14 +0700

    Add-WantToReadForm

--- src/main/resources/static/js/toprated.js
+++ src/main/resources/static/js/toprated.js
@@ -8,3 +8,15 @@ const changeDescription = () => {
 }
 
 document.addEventListener("DOMContentLoaded", changeDescription);
+function showForm() {
+        var form = document.getElementById("wantToReadForm");
+        form.style.display = "block";
+        document.getElementById("overlay").style.display = "block";
+    }
+
+    function closeForm() {
+        var form = document.getElementById("wantToReadForm");
+        form.style.display = "none";
+        document.getElementById("overlay").style.display = "none";
+    }
+
