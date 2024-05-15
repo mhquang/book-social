bf3160150c82bd35fcfc54110306bf178918c590
mhquang <mhquang03@gmail.com>
2024-05-16 04:56:26 +0700

    Update Edit profile

--- src/main/java/se2/project/BookSocial/controller/MainController.java
+++ src/main/java/se2/project/BookSocial/controller/MainController.java
@@ -7,17 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import se2.project.BookSocial.model.Author;
-import se2.project.BookSocial.model.Book;
-import se2.project.BookSocial.model.Bookshelf;
-import se2.project.BookSocial.model.Genre;
-import se2.project.BookSocial.model.MyUserDetails;
-import se2.project.BookSocial.model.User;
-import se2.project.BookSocial.repository.BookRepository;
-import se2.project.BookSocial.repository.GenreRepository;
-import se2.project.BookSocial.repository.UserRepository;
+import se2.project.BookSocial.model.*;
 import se2.project.BookSocial.repository.*;
 
 import java.util.List;
@@ -26,6 +17,7 @@ import java.util.List;
 public class MainController {
     @Autowired
     UserRepository userRepository;
+
     @Autowired
     BookRepository bookRepository;
 
@@ -38,14 +30,11 @@ public class MainController {
     @Autowired
     BookshelfRepository bookshelfRepository;
 
-    @Autowired
-    UserRepository userRepository;
-
     @GetMapping("/")
     public String getHome(Model model) {
         List<Book> books = bookRepository.findAll();
         List<Bookshelf> bookshelves = bookshelfRepository.findAll();
-        model.addAttribute("books" , books);
+        model.addAttribute("books", books);
         model.addAttribute("bookshelves", bookshelves);
         return "home";
     }
@@ -53,20 +42,21 @@ public class MainController {
     @GetMapping("/browse")
     public String getBrowse(Model model) {
         List<Book> books = bookRepository.findAll();
-        model.addAttribute("books" , books);
+        model.addAttribute("books", books);
         return "browse";
     }
+
     @GetMapping("/mybooks")
     public String getMyBooks(Model model) {
         List<Book> books = bookRepository.findAll();
-        model.addAttribute("books" , books);
+        model.addAttribute("books", books);
         return "mybooks";
     }
 
     @GetMapping("/browse/toprated")
     public String getTopRatedBook(Model model) {
         List<Book> books = bookRepository.findAll();
-        model.addAttribute("books" , books);
+        model.addAttribute("books", books);
         return "toprated";
     }
 
@@ -75,31 +65,30 @@ public class MainController {
             @AuthenticationPrincipal MyUserDetails myUserDetails, Model model) {
         User user = userRepository.getById(myUserDetails.getId());
         model.addAttribute("user", user);
-        model.addAttribute("myUserDetails", myUserDetails);
         return "user";
     }
 
     @RequestMapping(value = "/saveUser")
-    public String saveUpdate(User user, BindingResult result) {
+    public String saveUpdate(@Valid User user, BindingResult result) {
         if (result.hasErrors()) {
             return "user";
         }
         userRepository.save(user);
-        return "user";
+        return "redirect:/user";
     }
 
 
     @GetMapping("/browse/trending")
     public String getTrending(Model model) {
         List<Book> books = bookRepository.findAll();
-        model.addAttribute("books" , books);
+        model.addAttribute("books", books);
         return "trending";
     }
 
     @GetMapping("/browse/genres")
     public String getGenres(Model model) {
         List<Genre> genres = genreRepository.findAll();
-        model.addAttribute("genres" , genres);
+        model.addAttribute("genres", genres);
         return "genres";
     }
 
